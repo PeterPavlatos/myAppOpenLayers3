2db067167df14b8b7117d33832fcbbc087af5c89
Peter Pavlatos <pavlatos.peter@gmail.com>
2019-03-10 09:45:22 -0400

    Reverted back

--- php/config.php
+++ php/config.php
@@ -1,7 +1,16 @@
 <?php
 
 /* CONNECTION TO wamp  db */
+$host = "localhost";
+$user = "root";
+$password = "password";
+$dbname = "openlayers";
 
+// CONNECTION TO peterpavlatos.ca db
+// $host = "localhost"; /* Host name */
+// $user = "ppavlatos2016"; /* User */
+// $password = "Apollo1855!!"; /* Password */
+// $dbname = "tapis_homa"; /* Database name */
 
 $con = mysqli_connect($host, $user, $password, $dbname);
 // Check connection
